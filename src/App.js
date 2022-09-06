e6c95f72d9ad168f89cd698e4447a2c1bc93a9dc
Otubusin Ademuyiwa <53128146+tay4real@users.noreply.github.com>
2022-09-06 11:46:50 +0100

    Upload Landon Website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Footer from "./components/Footer.js";
+import Header from "./components/Header.js";
+import Main from "./components/Main.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React is a perfect marriage of JavaScript and markup
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
